300cc722536e50991a4130cc9d6bc849cba36a3b
ingemar <ingemar@xox.se>
2024-12-06 00:38:09 +0100

    General overhaul

--- lib/color_puts.rb
+++ lib/color_puts.rb
@@ -1,43 +1,44 @@
-require "color_puts/version"
+# frozen_string_literal: true
 
 module ColorPuts
   COLORS = {
-    black: '0;30',
-    dblue: '0;34',
-    dgreen: '0;32',
-    dcyan: '0;36',
-    dred: '0;31',
-    dpurple: '0;35',
-    dgray: '1;30',
-    brown: '0;33',
-    gray: '0;37',
-    blue: '1;34',
-    green: '1;32',
-    cyan: '1;36',
-    red: '1;31',
-    purple: '1;35',
-    yellow: '1;33',
-    white: '1;37'
-  }
+    "black" => "0;30",
+    "dblue" => "0;34",
+    "dgreen" => "0;32",
+    "dcyan" => "0;36",
+    "dred" => "0;31",
+    "dpurple" => "0;35",
+    "dgray" => "1;30",
+    "brown" => "0;33",
+    "gray" => "0;37",
+    "blue" => "1;34",
+    "green" => "1;32",
+    "cyan" => "1;36",
+    "red" => "1;31",
+    "purple" => "1;35",
+    "yellow" => "1;33",
+    "white" => "1;37"
+  }.freeze
+
+  REGEXP = /%(#{COLORS.keys.join("|")})\{([^}]*)\}/
+  private_constant :REGEXP
+
+  TEMPLATE = "\e[%<color>sm%<text>s\e[0;39m"
+  private_constant :TEMPLATE
 
   # Example:
   #
-  #  cputs "There was a %green{field} and %blue{sky}."
+  #  colorize("There was a %green{field} and %blue{sky}.")
   #
-  def cputs(string = "")
-    regexp = /%(\w+)\{[^}]*./
-    text_regexp = /%\w+{(.*)}/
+  def colorize(string)
+    string
+      .dup
+      .gsub(REGEXP) do |match|
+        color, text = match.match(REGEXP).captures
 
-    string = string.dup
-
-    while m = string.match(regexp) do
-      string.gsub!(m.to_s) do |s|
-        color = ColorPuts::COLORS[m.captures.first.to_sym]
-        text = s.match(text_regexp).captures.first
-        "\e[#{color}m#{text}\e[0;39m"
+        TEMPLATE % {color: COLORS[color], text:}
       end
-    end
-
-    puts string
   end
+
+  def cputs(string = "") = puts colorize(string)
 end
